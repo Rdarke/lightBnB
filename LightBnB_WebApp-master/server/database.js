d9c1c5f07d2d8b52f0998e5814581033ddc04d76
Rdarke <rickydarke1@gmail.com>
2021-09-16 13:01:51 -0700

    Update reservations to use lightbnb database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -65,7 +65,7 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  
+
   return pool
   .query(`
     INSERT INTO users (name, email, password)
@@ -90,7 +90,27 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  // return getAllProperties(null, 2);
+  return pool
+     .query(`
+     SELECT properties.*, reservations.*, avg(rating) as average_rating
+     FROM reservations
+     JOIN properties ON reservations.property_id = properties.id
+     JOIN property_reviews ON properties.id = property_reviews.property_id
+     WHERE reservations.guest_id = $1
+     AND reservations.end_date < now()::date
+     GROUP BY properties.id, reservations.id
+     ORDER BY reservations.start_date
+     LIMIT $2;`, 
+     [`${guest_id}`, limit]
+     )
+     .then((result) => {
+       return result.rows;
+     })
+     .catch((err) => {
+       console.log(err.message);
+     });
+
 }
 exports.getAllReservations = getAllReservations;
 
